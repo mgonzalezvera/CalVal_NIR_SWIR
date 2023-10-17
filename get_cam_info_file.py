a5a630fdaea2300d5d83a781677c46be20a2fdd3
mgonzalezvera <mgonzalezvera@conae.gov.ar>
2023-10-17 10:44:58 -0300

    Initial commit

--- get_cam_info_file.py
+++ get_cam_info_file.py
@@ -19,7 +19,6 @@ data_dark = mat73.loadmat(files_darks[0])
 data_dark = data_dark['salida']
 average_image_dark = np.mean(data_dark['imagen'], axis=2)
 #
-
 files_rad = sorted(glob.glob(path+'Rad/Temp1/Lcal/Images/*.mat'))
 data_rad = mat73.loadmat(files_rad[0])
 data_rad = data_rad['salida']
